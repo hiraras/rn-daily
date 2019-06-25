5b67a6759216a3ae5002d9da4a2d739e508e78af
hiraras <kazilagui@163.com>
2019-06-25 15:37:45 +0800

    添加路由

---
+++ src/views/Index/components/NavigatorItem/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderRadius: 6,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    fontSize: 24,
+    marginBottom: 16
+  },
+});
--- src/views/Index/index.js
+++ src/views/Index/index.js
@@ -1,16 +1,34 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Styles from './styles';
+import { View, Text, FlatList, Alert } from 'react-native';
+import NavigatorItem from './components/NavigatorItem';
+import styles from './styles';
 
 class Index extends Component {
   static navigationOptions = {
     title: '路由',
   }
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        { id: 1, name: 'Home', },
+      ]
+    }
+  }
+
+  navigatorClickHandle = (pageName) => {
+    this.props.navigation.navigate(pageName);
+  }
+
   render() {
     return (
-      <View>
-        <Text>this is Index</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({item}) => <NavigatorItem {...item} onClick={this.navigatorClickHandle} />}
+          keyExtractor={(_, index) => String(index)}
+        />
       </View>
     )
   }
